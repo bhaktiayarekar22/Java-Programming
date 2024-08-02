ad933c4537b85b8bfa507d7127349d3afb8d7543
Bhakti Ayarekar <bhaktiayarekar22@gmail.com>
2024-08-02 12:09:02 +0530

    Add files via upload

---
+++ 39_Binary_Search_in_2D_Array.java
@@ -0,0 +1,38 @@
+
+import java.util.Arrays;
+// not apply binary search
+public class Binary_Search_in_2D_Array {
+    public static void main(String[] args) {
+        //Array sorted in column and row wise
+        int[][] arr ={
+            {10,20,30,40},
+            {15,25,35,45},
+            {28,29,37,49},
+            {33, 34,38, 50}
+        };
+        System.out.println(Arrays.toString(search(arr, 37)));
+    }
+    static int[] search(int[][] matrix, int target)
+    {
+        int r = 0;
+        int c = matrix.length - 1;
+
+        while (r <matrix.length && c>=0) {
+            if(matrix[r][c] == target)
+            {
+                return new int[]{r,c};
+            } 
+            if(matrix[r][c] < target)
+            {
+                r++;
+            }
+            else
+            {
+                c--;
+            }
+            
+        }
+        return new int[] {-1,-1};
+
+    }
+}
